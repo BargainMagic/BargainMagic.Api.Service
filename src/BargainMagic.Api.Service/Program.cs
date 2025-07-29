4b35839a81ee6cfcb43942ac7bde67af626b53ad
Josh Glover <25733402+josh-glover@users.noreply.github.com>
2025-07-29 00:34:46 -0500

    Add Swagger and OpenAPI

--- src/BargainMagic.Api.Service/Program.cs
+++ src/BargainMagic.Api.Service/Program.cs
@@ -4,9 +4,19 @@ using BargainMagic.Api.Service.Endpoints;
 
 var builder = WebApplication.CreateBuilder(args);
 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddSingleton<IGetSeasonHandler, GetSeasonHandler>();
 builder.Services.AddSingleton<IGetSeasonsHandler, GetSeasonsHandler>();
 
 var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 EndpointBundle.MapEndpoints(app);
 
